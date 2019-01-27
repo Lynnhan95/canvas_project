5446535cdc4c09648ae7419b21631e0b9c6b3cca
你的英文名Lynnhan95 <han536@purdue.edu>
2019-01-27 12:17:43 -0500

    2

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ window.onresize = function(){
 
 var ctx = canvas1.getContext('2d')
 var paint = false;
-var using = false;
 var lastPoint = {x: undefined, y: undefined}
 
 if(document.body.ontouchstart !== undefined){
@@ -26,7 +25,7 @@ if(document.body.ontouchstart !== undefined){
         paint= true;
         console.log(ppp)
         if(eraserEnabled){
-            if(using){
+            if(paint){
                 ctx.clearRect(x, y, 10, 10)
             }
             
@@ -44,7 +43,7 @@ if(document.body.ontouchstart !== undefined){
         var y = kkk.touches[0].clientY;
         // console.log(x, y)
         if(eraserEnabled){
-            if(using){
+            if(paint){
                 ctx.clearRect(x, y, 10, 10)
             }
             
@@ -70,13 +69,12 @@ if(document.body.ontouchstart !== undefined){
         var x = eee.clientX;
         var y = eee.clientY;
         if(eraserEnabled){
-            using =true;
+            paint =true;
             ctx.clearRect(x-5, y-5, 10, 10)
         }else{
             paint = true;
             lastPoint = {'x':x, 'y':y }
         }
-        console.log(using)
     }
     
     // //move a mouse
@@ -84,7 +82,7 @@ if(document.body.ontouchstart !== undefined){
         var x = kkk.clientX;
         var y = kkk.clientY;
         if(eraserEnabled){
-            if(using){
+            if(paint){
                 ctx.clearRect(x, y, 10, 10)
             }
             
@@ -106,10 +104,32 @@ if(document.body.ontouchstart !== undefined){
 }
 
 //function drawline
+var lineWidth = 5;
+thin.onclick = function(){
+    lineWidth = 2
+}
+thick.onclick = function(){
+    lineWidth = 5
+}
+big.onclick = function(){
+    lineWidth = 10
+}
+clear.onclick = function(){
+    ctx.clearRect(0, 0, canvas1.width, canvas1.width)
+}
+save.onclick = function(){
+    var url = canvas1.toDataURL('image/png')
+    console.log(url)
+    var a = document.createElement('a')
+    document.body.appendChild(a)
+    a.href = url
+    a.download = 'myPainting'
+    a.click()
+}
 function drawLine(x1,y1,x2,y2){
     ctx.beginPath()
     ctx.moveTo(x1,y1)   //start point
-    ctx.lineWidth = 5
+    ctx.lineWidth = lineWidth
     ctx.lineTo(x2,y2)   //end point
     ctx.stroke()
     ctx.closePath()
@@ -119,13 +139,57 @@ var eraserEnabled = false;
 var actions = document.getElementById('actions')
 var eraser = document.getElementById('eraser')
 var brush = document.getElementById('brush')
-eraser.onclick = function(){
-    eraserEnabled = true;
-    actions.className="actions x"
-}
+// eraser.onclick = function(){
+//     eraserEnabled = true;
+//     actions.className="actions x"
+// }
+
+// brush.onclick = function(){
+//     eraserEnabled = false;
+//     actions.className="actions"
+// }
 
 brush.onclick = function(){
     eraserEnabled = false;
-    actions.className="actions"
+    brush.classList.add('active')
+    eraser.classList.remove('active')
 }
 
+eraser.onclick = function(){
+    eraserEnabled = true;
+    eraser.classList.add('active')
+    brush.classList.remove('active')
+
+}
+
+black.onclick = function(){
+    ctx.strokeStyle = 'black';
+    black.classList.add('active')
+    red.classList.remove('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+}
+
+red.onclick = function(){
+    ctx.strokeStyle = 'red';
+    red.classList.add('active')
+    green.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.remove('active')
+}
+
+green.onclick = function(){
+    ctx.strokeStyle = 'green';
+    green.classList.add('active')
+    red.classList.remove('active')
+    blue.classList.remove('active')
+    black.classList.remove('active')
+}
+
+blue.onclick = function(){
+    ctx.strokeStyle = 'blue ';
+    blue.classList.add('active')
+    green.classList.remove('active')
+    red.classList.remove('active')
+    black.classList.remove('active')
+}
